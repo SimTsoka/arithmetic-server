944e2ae3b9e4e18c1efa96e63ca51ded5f054b32
Magabole Tsoka <mtsoka021@student.wethinkcode.co.za>
2022-08-31 15:23:34 +0200

    Server can connect to client

--- Arithmetic/src/main/java/Server/Server.java
+++ Arithmetic/src/main/java/Server/Server.java
@@ -1,4 +1,36 @@
 package Server;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable{
+    private final int DEFAULT_PORT = 5000;
+    private ServerSocket serverSocket;
+    private Socket client;
+    @Override
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Server running");
+
+        while(true) {
+            try {
+                client = serverSocket.accept();
+                System.out.println("Client connected: " + client.isConnected());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
 }
