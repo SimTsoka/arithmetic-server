16e88acbea7de78679684024aed2961a9ab2a85a
Magabole Tsoka <mtsoka021@student.wethinkcode.co.za>
2022-08-31 15:11:44 +0200

    Created connection test

---
+++ Arithmetic/src/test/java/ServerTests/ConnectionTests.java
@@ -0,0 +1,29 @@
+package ServerTests;
+
+import Client.TestClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConnectionTests {
+    private final static int PORT = 5000;
+    private final static String IP = "localhost";
+    private final TestClient testClient = new TestClient();
+
+    @BeforeEach
+    void connectToServer() {
+        testClient.connect(IP, PORT);
+    }
+
+    @AfterEach
+    void disconnectFromServer() {
+        testClient.disconnect();
+    }
+
+    @Test
+    void testConnection() {
+        assertTrue(testClient.isConnected());
+    }
+}
