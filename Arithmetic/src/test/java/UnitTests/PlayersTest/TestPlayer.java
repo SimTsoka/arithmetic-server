a0bf8f8b1c9e612921658b591a8ba5f5e54b0ba2
Simon Tsoka <s.tsoka@gmail.com>
2022-09-01 18:09:32 +0200

    Added Player class

---
+++ Arithmetic/src/test/java/UnitTests/PlayersTest/TestPlayer.java
@@ -0,0 +1,13 @@
+package UnitTests.PlayersTest;
+
+import Players.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPlayer {
+    @Test
+    void testPlayerName() {
+        assertEquals("Simon", new Player("Simon").getName());
+    }
+}
