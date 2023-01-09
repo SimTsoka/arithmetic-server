d73a5b348ff491c724c80284b5161d805c172678
Simon Tsoka <s.tsoka@gmail.com>
2023-01-09 16:32:02 +0200

    updated execute test in ShutdownCommandTests to check for output.

--- Arithmetic/src/test/java/UnitTests/ServerTests/CommandsTests/ShutdownCommandTests.java
+++ Arithmetic/src/test/java/UnitTests/ServerTests/CommandsTests/ShutdownCommandTests.java
@@ -1,12 +1,31 @@
 package UnitTests.ServerTests.CommandsTests;
 
+import Players.PlayerDatabase;
 import Server.Commands.ShutdownCommand;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ShutdownCommandTests {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    // Create a class that creates outputStream also resets everything
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
 
     @Test
     void initialiseShutdownCommand() {
@@ -16,5 +35,6 @@ public class ShutdownCommandTests {
     @Test
     void testExecute() {
         assertFalse(new ShutdownCommand().execute());
+        assertEquals("Shutting Down..", outputStream.toString().trim());
     }
 }
