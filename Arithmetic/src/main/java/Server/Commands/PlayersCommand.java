7f137371686f56b112ef5c50843af96f5ed88577
Simon Tsoka <s.tsoka@gmail.com>
2023-01-09 16:23:59 +0200

    PlayersCommand now shows list of players. Tests included.

--- Arithmetic/src/main/java/Server/Commands/PlayersCommand.java
+++ Arithmetic/src/main/java/Server/Commands/PlayersCommand.java
@@ -1,5 +1,10 @@
 package Server.Commands;
 
+import Players.Player;
+import Players.PlayerDatabase;
+
+import java.util.HashMap;
+
 public class PlayersCommand extends ServerCommand{
 
     public PlayersCommand() {
@@ -8,6 +13,20 @@ public class PlayersCommand extends ServerCommand{
 
     @Override
     public boolean execute() {
-        return false;
+
+        HashMap<String, Player> players = PlayerDatabase.getPlayers();
+
+        if (players.isEmpty()) {
+            System.out.println("No players found.");
+        } else {
+            System.out.println("Number of players = "+players.keySet().size());
+            System.out.println("Players:");
+
+            for (String name: players.keySet().stream().sorted().toList()) {
+                System.out.println(name);
+            }
+        }
+
+        return true;
     }
 }
--- Arithmetic/src/test/java/UnitTests/ServerTests/CommandsTests/PlayersCommandTests.java
+++ Arithmetic/src/test/java/UnitTests/ServerTests/CommandsTests/PlayersCommandTests.java
@@ -1,13 +1,56 @@
 package UnitTests.ServerTests.CommandsTests;
 
+import Players.Player;
+import Players.PlayerDatabase;
 import Server.Commands.PlayersCommand;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayersCommandTests {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        PlayerDatabase.reset();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    @AfterAll
+    static void afterEveryTest() {
+        PlayerDatabase.reset();
+    }
+
     @Test
     void initialisePlayersCommand() {
         assertEquals("players", new PlayersCommand().getName());
+    }
+
+    @Test
+    void testExecuteWithNoPlayers() {
+        assertTrue(new PlayersCommand().execute());
+        assertEquals("No players found.", outputStream.toString().trim());
+    }
+
+    @Test
+    void testExecuteWithPlayers() {
+        PlayerDatabase.addNewPlayer(new Player("Simon"));
+        PlayerDatabase.addNewPlayer(new Player("Jane"));
+
+        assertTrue(new PlayersCommand().execute());
+        assertEquals("Number of players = 2\n" +
+                "Players:\nJane\nSimon",outputStream.toString().trim());
     }
 }
